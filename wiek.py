3e99fd60fd3135e754ade79afaffe9956bf80c7b
lukasz.krause <lukasz.krause@onet.eu>
2017-11-17 11:45:48 +0100

    dodanie komentarza operatorów matematycznych

--- wiek.py
+++ wiek.py
@@ -9,7 +9,7 @@ print('Dzisiaj jest:  :', today.ctime())
 tt = today.timetuple()
 wiek = (tt.tm_year - rok)
 print('Jeśli teraz mamy rok', tt.tm_year, 'to obecnie masz', wiek , 'lata')
-print("Przyjmując że rok ma 365 dni to żyjesz już około:", wiek * 365)
+print("Przyjmując że rok ma 365 dni to żyjesz już około:", wiek * 365) #operatory matematyczne -> // dzielenie z wynikiem całkowitym
 print("\a\n\t\t Miłego dnia życzę") # \t zmak tabulacji w Python \n znak nowego wiersza
 input("\n\nAby zakończyć program, naciśnij klawisz \"Enter\".") #\" lub ' cudzysłów aby Python nie traktował jako końca łancucha
 
