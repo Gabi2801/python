1cfb814d9f4f3df00d443b9c80ae97eb6e94aeb7
lukasz.krause <lukasz.krause@onet.eu>
2017-11-17 11:32:41 +0100

    Dodanie pliku

---
+++ wiek.py
@@ -0,0 +1,14 @@
+x = input("Podaj swoje imię: ") #komentarz 
+print ( "Witaj", x)
+rok = int(input("Podaj swój rok urodzenia: "))
+
+import datetime
+
+today = datetime.date.today()
+print('Dzisiaj jest:  :', today.ctime())
+tt = today.timetuple()
+wiek = (tt.tm_year - rok)
+print('Jeśli teraz mamy rok', tt.tm_year, 'to obecnie masz', wiek , 'lata')
+print("\a\n\t\t Miłego dnia życzę") # \t zmak tabulacji w Python \n znak nowego wiersza
+input("\n\nAby zakończyć program, naciśnij klawisz \"Enter\".") #\" lub ' cudzysłów aby Python nie traktował jako końca łancucha
+
