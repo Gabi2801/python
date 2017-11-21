665cb1a5f1ec796c7f4ab50b4c7942378fb49a59
lukasz.krause <lukasz.krause@onet.eu>
2017-11-21 21:36:21 +0100

    Angielskie nazwy zmiennych

--- wiek.py
+++ wiek.py
@@ -1,15 +1,16 @@
-x = input("Podaj swoje imię: ") #komentarz 
-print ( "Witaj", x)
-rok = int(input("Podaj swój rok urodzenia: "))
-
+name = input("Podaj swoje imię: ") #komentarz 
+print ( "Witaj", name)
+year = int(input("Podaj swój rok urodzenia: "))
+#nazwa zmiennej nie może zaczynać się od cyfry
+#
 import datetime
 
 today = datetime.date.today()
 print('Dzisiaj jest:  :', today.ctime())
 tt = today.timetuple()
-wiek = (tt.tm_year - rok)
-print('Jeśli teraz mamy rok', tt.tm_year, 'to obecnie masz', wiek , 'lata')
-print("Przyjmując że rok ma 365 dni to żyjesz już około:", wiek * 365) #operatory matematyczne -> // dzielenie z wynikiem całkowitym
+old = (tt.tm_year - year)
+print('Jeśli teraz mamy rok', tt.tm_year, 'to obecnie masz', old , 'lata')
+print("Przyjmując że rok ma 365 dni to żyjesz już około:", old * 365) #operatory matematyczne -> // dzielenie z wynikiem całkowitym
 print("\a\n\t\t Miłego dnia życzę") # \t zmak tabulacji w Python \n znak nowego wiersza
 input("\n\nAby zakończyć program, naciśnij klawisz \"Enter\".") #\" lub ' cudzysłów aby Python nie traktował jako końca łancucha
 
