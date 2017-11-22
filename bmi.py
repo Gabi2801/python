7752a087491e202c4fdceb6d93cacacd5b94c7dc
lukasz.krause <lukasz.krause@onet.eu>
2017-11-22 22:19:33 +0100

    Dodanie kodu programu BMI

--- bmi.py
+++ bmi.py
@@ -1,12 +1,26 @@
-name = input('Podaj swoje imię:"')
-print('Cześć ', name)
-weight = int(input("Ile kilogramów ważysz? "))
-height = int(input("Ile masz wzrostu? "))
-sum = weight / height
+language = input('Please select your language: \n EN English \n PL - Polish \n')
 
-if sum > 18-25:
-    print("Waga prawdidłowa")
-if sum > 26:
-    print("Masz nadwagę")
+if language == 'pl':
+    height = float(input('Podaj swój wzrost w cm: '))
+    weight = int(input('Podaj swoją wagę w kg: '))
+    bmi = weight /(height / 100 *height / 100)
+    
+    if bmi <= 18.5:
+        
+        print('Twoje BMI:', bmi,' czyli masz niedowagę.')
 
-input("\n\nAby zakończyć program, naciśnij klawisz \"Enter\".")
+    elif bmi > 18.5 and bmi < 25:
+        print('Twoje BMI:', bmi,' czyli Twoja waga jest prawidłowa')
+
+    elif bmi > 25 and bmi < 30:
+        print('Twoje BMI:', bmi,' masz nadwagę.')
+
+    elif bmi > 30:
+        print('Twoje BMI:', bmi,' Uwaga - Twoja waga wskazuje otyłość')
+
+    else:
+        print('Podałeś nieprawidłowe dane')
+    
+
+
+input('\n\nAby zakończyć naciśnij Enter.')
