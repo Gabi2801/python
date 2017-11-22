db4cf3fbda024c6786852ea45c291bd9afda54bd
lukasz.krause <lukasz.krause@onet.eu>
2017-11-22 21:28:29 +0100

    Projekt programu BMI

---
+++ bmi.py
@@ -0,0 +1,12 @@
+name = input('Podaj swoje imię:"')
+print('Cześć ', name)
+weight = int(input("Ile kilogramów ważysz? "))
+height = int(input("Ile masz wzrostu? "))
+sum = weight / height
+
+if sum > 18-25:
+    print("Waga prawdidłowa")
+if sum > 26:
+    print("Masz nadwagę")
+
+input("\n\nAby zakończyć program, naciśnij klawisz \"Enter\".") #\" lub ' cudzysłów aby Python nie traktował jako końca łancucha
