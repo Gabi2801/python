e875899ce763c9832d3075b676d40f329ae7e979
lukasz.krause <lukasz.krause@onet.eu>
2017-11-22 22:31:44 +0100

    Poprawa kodu

--- bmi1.py
+++ bmi1.py
@@ -21,3 +21,31 @@ if language == 'pl':
     else:
         print('Podałeś nieprawidłowe dane')
     
+
+
+
+if language == 'en':
+    height = float(input('Please enter your height input meters(decimals): '))
+    weight = int(input('Please enter your weight input kg: '))
+    bmi = weight /(height *height)
+    
+    if bmi <= 18.5:
+        
+        print('Your BMI is', bmi,'which means you are underweight.')
+
+    elif bmi > 18.5 and bmi < 25:
+        print('Your BMI is', bmi,'which means you are normal.')
+
+    elif bmi > 25 and bmi < 30:
+        print('your BMI is', bmi,'overweight.')
+
+    elif bmi > 30:
+        print('Your BMI is', bmi,'which means you are obese.')
+
+    else:
+        print('There is an error with your input')
+        print('Please check you have entered whole numbers\n'
+              'and decimals were asked.')
+
+
+input('\n\nPlease press enter to exit. \nAby zakończyć naciśnij Enter')
